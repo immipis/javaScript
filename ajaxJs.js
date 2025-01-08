95bf7e22612cbc3cd12ba3f88f86321cf26e3aed
immipis <132124793+immipis@users.noreply.github.com>
2025-01-08 12:50:55 +0900

    2025 01 08
    적당히 추가 삭제 등등

--- ajaxJs.js
+++ ajaxJs.js
@@ -3,10 +3,19 @@ init();
 //최초셋팅
 function init() {
   //각 태그의 이벤트 처리
+  document.querySelector('#initBtn').addEventListener('click',e=>{
+    document.querySelectorAll('#info input, #info select').forEach(element => { //입력 태그를 검색 : input, select
+      element.value = "";
+    });
+  })
 
+  document.querySelector('#insertBtn').addEventListener('click',addUserInfo)
+  document.querySelector('#updateBtn').addEventListener('click',updateUserInfo)
+  document.querySelector('#delBtn').addEventListener('click',delUserInfo)
   //데이터를 가져오는 작업
   getUserList();
 }
+
 
 function getUserList() {
   //회원데이터 전체 조회
@@ -66,6 +75,7 @@ function addTbody(list){
 }
 */
 function addTbody2(list){
+  document.querySelector('#list tbody').replaceChildren();
   list.forEach(element => {
     let trTag = addtrTag(element)
     document.querySelector('#list tbody').append(trTag)
@@ -84,4 +94,62 @@ function addtrTag(info){
     trTag.append(tdTag)
   }
   return trTag
+}
+function addUserInfo(event){  //새로운 회원 등록
+  //새로운 회원 정보
+  let userInfo = formUserInfo();
+  // 서버에 전송 : ajax
+  fetch("http://192.168.0.11:8099/userInsert", {
+    method: "POST",
+    /* "Content-Type" 마다 데이터가 다름 
+      application/x-www-form-urlencoded  = body : new URLSearchParams(userInfo) 
+    */
+    body: new URLSearchParams(userInfo)
+  })
+  .then(response => response.json())
+  .then(result =>{
+    document.getElementsByName('no')[0].value = result.no
+    getUserList();
+   })
+  .catch(err => console.log(err))
+
+}
+function formUserInfo(){
+  let obj={};
+  document.querySelectorAll('#info input, #info select').forEach(element => {
+    obj[element.name] =element.value;
+  });
+  return obj;
+}
+
+function updateUserInfo(event){  //새로운 회원 등록
+  let userInfo = formUserInfo();
+  fetch("http://192.168.0.11:8099/userUpdate", {
+    method: "POST",
+    /* "Content-Type" 마다 데이터가 다름 
+      application/json = body : JSON.stringify(userInfo)
+    */
+    headers : {
+      "Content-Type" : "application/json"
+    },
+    body: JSON.stringify(userInfo)
+  })
+  .then(response => response.json())
+  .then(result =>{
+    getUserList();
+   })
+  .catch(err => console.log(err))
+
+}
+function delUserInfo(event){
+  let userInfo = formUserInfo();
+  fetch(`http://192.168.0.11:8099/userDelete?id=${userInfo.id}`)
+  .then(response => response.json())
+  .then(result =>{
+    getUserList();
+    document.querySelectorAll('#info input, #info select').forEach(element => {
+      element.value = "";
+    });
+   })
+  .catch(err => console.log(err))
 }
